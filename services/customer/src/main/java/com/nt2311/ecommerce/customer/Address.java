c529cebade6d35341f389e12d2f107761603b322
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-20 22:33:22 +0700

    complete the address and customer class without lombok annotation

--- services/customer/src/main/java/com/nt2311/ecommerce/customer/Address.java
+++ services/customer/src/main/java/com/nt2311/ecommerce/customer/Address.java
@@ -1,23 +1,75 @@
 package com.nt2311.ecommerce.customer;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.validation.annotation.Validated;
 
 /** Address */
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Getter
-@Setter
 @Document
 @Validated
 public class Address {
   private String address;
   private String houseNumber;
   private String zipCode;
+
+  public Address() {
+  }
+
+  public Address(String address, String houseNumber, String zipCode) {
+    this.address = address;
+    this.houseNumber = houseNumber;
+    this.zipCode = zipCode;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setHouseNumber(String houseNumber) {
+    this.houseNumber = houseNumber;
+  }
+
+  public String getHouseNumber() {
+    return houseNumber;
+  }
+
+  public String getZipCode() {
+    return zipCode;
+  }
+
+  public void setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+  }
+
+  public static class Builder {
+    private String address;
+    private String houseNumber;
+    private String zipCode;
+
+    public Builder address(String address) {
+      this.address = address;
+      return this;
+    }
+
+    public Builder houseNumber(String houseNumber) {
+      this.houseNumber = houseNumber;
+      return this;
+    }
+
+    public Builder zipCode(String zipCode) {
+      this.zipCode = zipCode;
+      return this;
+    }
+
+    public Address build() {
+      return new Address(address, houseNumber, zipCode);
+    }
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
 }
--- services/customer/src/main/java/com/nt2311/ecommerce/customer/Customer.java
+++ services/customer/src/main/java/com/nt2311/ecommerce/customer/Customer.java
@@ -1,18 +1,10 @@
 package com.nt2311.ecommerce.customer;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
-@Getter
-@Setter
 @Document
 /** Customer */
 public class Customer {
@@ -22,4 +14,96 @@ public class Customer {
   private String lastName;
   private String email;
   private Address address;
+
+  public Customer() {
+  }
+
+  public Customer(String id, String firstName, String lastName, String email, Address address) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.address = address;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Address getAddress() {
+    return address;
+  }
+
+  public void setAddress(Address address) {
+    this.address = address;
+  }
+
+  public static class Builder {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Address address;
+
+    public Builder id(String id) {
+      this.id = id;
+      return this;
+    }
+
+    public Builder firstName(String firstName) {
+      this.firstName = firstName;
+      return this;
+    }
+
+    public Builder lastName(String lastName) {
+      this.lastName = lastName;
+      return this;
+    }
+
+    public Builder email(String email) {
+      this.email = email;
+      return this;
+    }
+
+    public Builder address(Address address) {
+      this.address = address;
+      return this;
+    }
+
+    public Customer build() {
+      return new Customer(id, firstName, lastName, email, address);
+    }
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
 }
