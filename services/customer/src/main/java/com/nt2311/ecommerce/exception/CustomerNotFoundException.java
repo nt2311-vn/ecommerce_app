b1b5f48f887986a73cdced8b1c2c63b5bd99f2c0
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-21 22:25:33 +0700

    rewrite without lombok annotations

--- services/customer/src/main/java/com/nt2311/ecommerce/exception/CustomerNotFoundException.java
+++ services/customer/src/main/java/com/nt2311/ecommerce/exception/CustomerNotFoundException.java
@@ -1,11 +1,38 @@
 package com.nt2311.ecommerce.exception;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
 /** CustomerNotFoundException */
-@EqualsAndHashCode(callSuper = true)
-@Data
 public class CustomerNotFoundException extends RuntimeException {
   private final String msg;
+
+  public CustomerNotFoundException(String msg) {
+    super(msg);
+    this.msg = msg;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    CustomerNotFoundException that = (CustomerNotFoundException) o;
+
+    return msg != null ? msg.equals(that.msg) : that.msg == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = super.hashCode();
+    result = 31 + result + (msg != null ? msg.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return getClass().getSimpleName() + "[msg=" + msg + "]";
+  }
 }
