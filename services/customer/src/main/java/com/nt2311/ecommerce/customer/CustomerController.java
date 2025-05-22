90e4ba3e75456f60950b4e83088b2e74fd3d8d87
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-22 22:23:10 +0700

    controller of existsById and findById

--- services/customer/src/main/java/com/nt2311/ecommerce/customer/CustomerController.java
+++ services/customer/src/main/java/com/nt2311/ecommerce/customer/CustomerController.java
@@ -1,8 +1,10 @@
 package com.nt2311.ecommerce.customer;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,6 +35,17 @@ public class CustomerController {
   }
 
   @GetMapping
-  public ResponseEntity<List<CustomerResponse>> findAdd() {
+  public ResponseEntity<List<CustomerResponse>> findAll() {
+    return ResponseEntity.ok(service.findAllCustomers());
+  }
+
+  @GetMapping("/exists/{customer-id}")
+  public ResponseEntity<Boolean> existsById(@PathVariable("customer-id") String customerId) {
+    return ResponseEntity.ok(service.existsById(customerId));
+  }
+
+  @GetMapping("/{customer-id}")
+  public ResponseEntity<CustomerResponse> findById(@PathVariable("customer-id") String customerId) {
+    return ResponseEntity.ok(service.findById(customerId));
   }
 }
