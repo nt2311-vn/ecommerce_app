680ff74364192de0944311107987c38cbf6d1715
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-22 22:22:40 +0700

    write method to check customer exist and find customer by Id

--- services/customer/src/main/java/com/nt2311/ecommerce/customer/CustomerService.java
+++ services/customer/src/main/java/com/nt2311/ecommerce/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.nt2311.ecommerce.customer;
 
 import com.nt2311.ecommerce.exception.CustomerNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
@@ -45,5 +47,22 @@ public class CustomerService {
     if (request.address() != null) {
       customer.setAddress(request.address());
     }
+  }
+
+  public List<CustomerResponse> findAllCustomers() {
+    return repository.findAll().stream().map(mapper::fromCustomer).collect(Collectors.toList());
+  }
+
+  public Boolean existsById(String customerId) {
+    return repository.findById(customerId).isPresent();
+  }
+
+  public CustomerResponse findById(String customerId) {
+    return repository
+        .findById(customerId)
+        .map(mapper::fromCustomer)
+        .orElseThrow(
+            () -> new CustomerNotFoundException(
+                String.format("No customer found with the prrovided ID:: %s", customerId)));
   }
 }
