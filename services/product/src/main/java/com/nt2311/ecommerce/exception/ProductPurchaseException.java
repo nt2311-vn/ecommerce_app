da1578aebd376a8c18db1bd1263d7cdc0ee772ae
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-25 15:55:00 +0700

    create exception for product purchase service

---
+++ services/product/src/main/java/com/nt2311/ecommerce/exception/ProductPurchaseException.java
@@ -0,0 +1,8 @@
+package com.nt2311.ecommerce.exception;
+
+/** ProductPurchaseException */
+public class ProductPurchaseException extends RuntimeException {
+  public ProductPurchaseException(String message) {
+    super(message);
+  }
+}
