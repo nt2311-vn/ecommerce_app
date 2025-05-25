3c8f5b74c7eb1b20ed2c543731d2a4b406f03038
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-25 20:51:21 +0700

    static class ProductPurchaseResponse

--- services/product/src/main/java/com/nt2311/ecommerce/product/ProductPurchaseResponse.java
+++ services/product/src/main/java/com/nt2311/ecommerce/product/ProductPurchaseResponse.java
@@ -1,5 +1,8 @@
 package com.nt2311.ecommerce.product;
 
+import java.math.BigDecimal;
+
 /** ProductPurchaseResponse */
-public record ProductPurchaseResponse() {
+public record ProductPurchaseResponse(
+    Integer productId, String name, String description, BigDecimal price, double quantity) {
 }
