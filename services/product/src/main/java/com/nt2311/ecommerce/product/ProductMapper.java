1aea5a8248819b0ad39fe7b100eae0f13c9a7a93
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-25 20:51:02 +0700

    write mapper to convert toProductPurchaseResponse

--- services/product/src/main/java/com/nt2311/ecommerce/product/ProductMapper.java
+++ services/product/src/main/java/com/nt2311/ecommerce/product/ProductMapper.java
@@ -29,4 +29,9 @@ public class ProductMapper {
         product.getCategory().getName(),
         product.getCategory().getDescription());
   }
+
+  public ProductPurchaseResponse toProductPurchaseResponse(Product product, double quantity) {
+    return new ProductPurchaseResponse(
+        product.getId(), product.getName(), product.getDescription(), product.getPrice(), quantity);
+  }
 }
